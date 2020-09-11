379d05d814ae432b60ba2d874b0218d373a40d31
Cosimo <cosimogiani95@gmail.com>
2020-09-11 12:03:45 +0200

    First commit

---
+++ com.prova.github/src/main/java/com/prova/github/App.java
@@ -0,0 +1,13 @@
+package com.prova.github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World bonobo!" );
+    }
+}
